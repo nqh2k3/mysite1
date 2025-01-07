2b7f9b0d9f1460d9d32a7031e86ec90eb0cd12bc
nqh2k3 <110041416+VinciHuy@users.noreply.github.com>
2025-01-07 17:02:39 +0700

    add -

--- db/migrate/20250107094147_create_solid_queue_jobs.rb
+++ db/migrate/20250107094147_create_solid_queue_jobs.rb
@@ -6,14 +6,10 @@ class CreateSolidQueueJobs < ActiveRecord::Migration[8.0]
       t.text :arguments
       t.datetime :scheduled_at
       t.datetime :performed_at
-      t.datetime :failed_at
-      t.string :error_message
-      t.integer :attempts, default: 0
 
       t.timestamps
     end
 
     add_index :solid_queue_jobs, :job_id, unique: true
-    add_index :solid_queue_jobs, :queue_name
   end
 end
